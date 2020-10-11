40d7344a2d04c0a877d33aa872c5a31ab4f844c6
ceceliacreates <ceceliacreates@gmail.com>
2020-10-11 17:02:12 -0400

    added functionality to view added projects on Folio page. Also updated mobile menu links and added navigation to Folio page.

--- cypress/integration/addProject.spec
+++ cypress/integration/addProject.spec
@@ -6,12 +6,14 @@ describe("adds project", function () {
         cy.visit("/");
         cy.contains("SIGN IN").click();
         cy.contains("Sign In to your Folio Account");
-        cy.get('[name=email]').click().type("test@email.com");
+        cy.get('[name=email]').click().type("test2@email.com");
         cy.get('[name=password]').click().type("123456");
         cy.get('[data-cy=submit]').click();
         cy.get('[data-cy=logoutButton]');
         cy.get('[data-cy=projectsPage]').click();
         cy.contains("SELECT").click();
+        cy.get('[data-cy=addProjectButton]').first().click();
+        cy.wait()
         cy.get('[data-cy=addProjectButton]').last().click();
     })
 })
--- src/views/Folio.vue
+++ src/views/Folio.vue
@@ -3,11 +3,54 @@
     <v-row>
       <h1>Folio</h1>
     </v-row>
+     <v-row wrap>
+      <v-col
+        cols="12"
+        sm="6"
+        lg="4"
+        v-for="(userProject, idx) in userProjects"
+        :key="idx"
+      >
+        <v-card>
+          <v-img :src="userProject.image"></v-img>
+
+          <v-card-text>
+            <div class="title">{{ userProject.name }}</div>
+
+            <div class="subheading text-uppercase cyan--text text--lighten-1">{{ userProject.type }}</div>
+            <v-btn
+              v-for="(tag, idx) in userProject.tags"
+              :key="idx"
+              small
+              depressed
+              text
+              disabled
+              class="mx-1"
+              >{{ tag }}</v-btn
+            >
+            <div class="text-body">{{ userProject.description }}</div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 <script>
 export default {
-  name: "Folio"
+    name: 'Folio',
+    computed: {
+        userProjects() {
+            return this.$store.state.userProjects;
+        }
+    },
+    mounted() {
+        this.getUserProjects();
+    },
+    methods: {
+        getUserProjects() {
+            this.$store.dispatch('getUserProjects');
+        }
+    }
 };
 </script>
 <style scoped></style>
